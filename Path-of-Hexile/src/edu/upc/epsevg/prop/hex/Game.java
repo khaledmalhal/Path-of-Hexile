398f193d6e66bc8b149380902d6865f094a832c2
jordimolo <161064565+jordimolo@users.noreply.github.com>
2024-12-22 12:18:23 +0100

    Add files via upload

---
+++ Path-of-Hexile/src/edu/upc/epsevg/prop/hex/Game.java
@@ -0,0 +1,37 @@
+package edu.upc.epsevg.prop.hex;
+
+import edu.upc.epsevg.prop.hex.players.HumanPlayer;
+import edu.upc.epsevg.prop.hex.players.RandomPlayer;
+import edu.upc.epsevg.prop.hex.players.PlayerID;
+import edu.upc.epsevg.prop.hex.IPlayer;
+import edu.upc.epsevg.prop.hex.IPlayer;
+import edu.upc.epsevg.prop.hex.IPlayer;
+import edu.upc.epsevg.prop.hex.players.H_E_X_Player;
+
+
+
+import javax.swing.SwingUtilities;
+
+/**
+ * Checkers: el joc de taula.
+ * @author bernat
+ */
+public class Game {
+        /**
+     * @param args
+     */
+    public static void main(String[] args) { 
+        
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                
+                IPlayer player2 = new H_E_X_Player(2/*GB*/);
+                
+                IPlayer player1 = new PlayerID(2);
+                                
+                new Board(player1 , player2, 11 /*mida*/,  10/*s*/, false);
+             }
+        });
+    }
+}
